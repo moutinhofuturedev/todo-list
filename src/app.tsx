c7d9d2cacea4b3e7373e8e7d963daee494dc12ef
paulo <paulo_vicali@icloud.com>
2024-10-13 20:26:04 -0300

    feat(main): add sonner

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { TodoList } from '@/components/todo-list'
 import { queryClient } from '@/lib/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 
 export const App = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<Toaster />
 			<TodoList />
 		</QueryClientProvider>
 	)
